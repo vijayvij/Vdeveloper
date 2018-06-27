fbfb3f973b950ef7480e4c6a8ff8e412b51a71ab
vijayvij <vhat007@gmail.com>
2018-06-27 08:38:35 +0530

    finally for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://vijay:V198258y@ds161740.mlab.com:61740/vconnector"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
